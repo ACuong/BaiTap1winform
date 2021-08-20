ce649983e5a9c3a70aa09e317c6557c4dc76d161
Nguyen Van Cuong <1521050560@student.humg.edu.vn>
2021-08-20 14:32:24 +0700

    Test cap nhat

--- BaiTap1winform/Form1.cs
+++ BaiTap1winform/Form1.cs
@@ -17,5 +17,14 @@ namespace BaiTap1winform
             InitializeComponent();
         }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
