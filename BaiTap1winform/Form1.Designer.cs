3684a602783bcd85e194d0e3adb75bec7db6e7e6
Nguyen Van Cuong <1521050560@student.humg.edu.vn>
2021-08-22 21:47:13 +0700

    giao dien form

--- BaiTap1winform/Form1.Designer.cs
+++ BaiTap1winform/Form1.Designer.cs
@@ -33,51 +33,162 @@ namespace BaiTap1winform
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnTiepTuc = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnNhap = new System.Windows.Forms.Button();
+            this.txtNhap = new System.Windows.Forms.TextBox();
+            this.txtDayNhap = new System.Windows.Forms.TextBox();
+            this.txtTongPhanTu = new System.Windows.Forms.TextBox();
+            this.txtTongChan = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(138, 83);
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(51, 123);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 20);
+            this.label1.Size = new System.Drawing.Size(93, 28);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Nhập số";
+            this.label1.Text = "Nhập số:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(138, 122);
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(51, 165);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(100, 20);
+            this.label2.Size = new System.Drawing.Size(141, 28);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Dãy vừa nhập";
+            this.label2.Text = "Dãy vừa nhập:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(138, 166);
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(51, 212);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(192, 20);
+            this.label3.Size = new System.Drawing.Size(269, 28);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Tổng các phần tử trong dãy";
+            this.label3.Text = "Tổng các phần tử trong dãy:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(138, 222);
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(51, 259);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(78, 20);
+            this.label4.Size = new System.Drawing.Size(116, 28);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Tổng chẵn";
+            this.label4.Text = "Tổng Chẵn:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(76, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(367, 38);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Nhập Dãy Số và Tính Tổng";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(259, 259);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 28);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Tổng Lẻ:";
+            // 
+            // btnTiepTuc
+            // 
+            this.btnTiepTuc.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnTiepTuc.Location = new System.Drawing.Point(106, 319);
+            this.btnTiepTuc.Name = "btnTiepTuc";
+            this.btnTiepTuc.Size = new System.Drawing.Size(115, 38);
+            this.btnTiepTuc.TabIndex = 5;
+            this.btnTiepTuc.Text = "Tiếp tục";
+            this.btnTiepTuc.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnThoat.Location = new System.Drawing.Point(281, 319);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(115, 38);
+            this.btnThoat.TabIndex = 5;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // btnNhap
+            // 
+            this.btnNhap.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnNhap.Location = new System.Drawing.Point(328, 119);
+            this.btnNhap.Name = "btnNhap";
+            this.btnNhap.Size = new System.Drawing.Size(104, 38);
+            this.btnNhap.TabIndex = 5;
+            this.btnNhap.Text = "Nhập";
+            this.btnNhap.UseVisualStyleBackColor = true;
+            // 
+            // txtNhap
+            // 
+            this.txtNhap.Location = new System.Drawing.Point(198, 126);
+            this.txtNhap.Name = "txtNhap";
+            this.txtNhap.Size = new System.Drawing.Size(111, 27);
+            this.txtNhap.TabIndex = 6;
+            // 
+            // txtDayNhap
+            // 
+            this.txtDayNhap.Location = new System.Drawing.Point(198, 168);
+            this.txtDayNhap.Name = "txtDayNhap";
+            this.txtDayNhap.Size = new System.Drawing.Size(234, 27);
+            this.txtDayNhap.TabIndex = 6;
+            // 
+            // txtTongPhanTu
+            // 
+            this.txtTongPhanTu.Location = new System.Drawing.Point(328, 216);
+            this.txtTongPhanTu.Name = "txtTongPhanTu";
+            this.txtTongPhanTu.Size = new System.Drawing.Size(104, 27);
+            this.txtTongPhanTu.TabIndex = 6;
+            // 
+            // txtTongChan
+            // 
+            this.txtTongChan.Location = new System.Drawing.Point(176, 262);
+            this.txtTongChan.Name = "txtTongChan";
+            this.txtTongChan.Size = new System.Drawing.Size(73, 27);
+            this.txtTongChan.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(356, 261);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(76, 27);
+            this.textBox1.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(512, 419);
+            this.Controls.Add(this.txtDayNhap);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtTongChan);
+            this.Controls.Add(this.txtTongPhanTu);
+            this.Controls.Add(this.txtNhap);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnNhap);
+            this.Controls.Add(this.btnTiepTuc);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -95,6 +206,16 @@ namespace BaiTap1winform
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnTiepTuc;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnNhap;
+        private System.Windows.Forms.TextBox txtNhap;
+        private System.Windows.Forms.TextBox txtDayNhap;
+        private System.Windows.Forms.TextBox txtTongPhanTu;
+        private System.Windows.Forms.TextBox txtTongChan;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- BaiTap1winform/Form1.cs
+++ BaiTap1winform/Form1.cs
@@ -26,5 +26,10 @@ namespace BaiTap1winform
         {
 
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
