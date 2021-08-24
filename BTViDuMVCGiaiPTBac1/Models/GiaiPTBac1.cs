17b77dd0b9d3a4ffa071c9b9c1dea285c2df863d
Nguyen Van Cuong <1521050560@student.humg.edu.vn>
2021-08-24 14:13:42 +0700

    SuaProjectC#

---
+++ BTViDuMVCGiaiPTBac1/Models/GiaiPTBac1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BTViDuMVCGiaiPTBac1.Models
+{
+    public class GiaiPTBac1
+    {
+        public Double GiaiPTBacMot(Double a, Double b)
+        {
+            Double x;
+            x = -b / a;
+            return x;
+        }
+    }
+}
