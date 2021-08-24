e99376829fa6c3a20ec620f2baad059676c9d5ba
Nguyen Van Cuong <1521050560@student.humg.edu.vn>
2021-08-24 13:58:59 +0700

    TaoDemoController

---
+++ BaiTapViDuMVC/Controllers/DemoController.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class DemoController
+        Inherits Controller
+
+        ' GET: Demo
+        Function Index() As ActionResult
+            Return View()
+        End Function
+
+        Function Hello() As ActionResult
+            Return View()
+        End Function
+    End Class
+End Namespace
---
+++ BaiTapViDuMVC/Views/Demo/Hello.vbhtml
@@ -0,0 +1,7 @@
+﻿@Code
+    ViewData("Title") = "Hello"
+    Layout = "~/Views/Shared/_Layout.vbhtml"
+End Code
+
+<h1>DemoController,View Hello</h1>
+
